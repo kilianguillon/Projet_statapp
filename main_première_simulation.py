2305523465e1c0671b2c9a782569f2729e33d09f
kilianguillon <kilianguillon@gmail.com>
2024-02-06 13:46:18 +0000

    change

---
+++ main_première_simulation.py
@@ -0,0 +1,133 @@
+#Librairies
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import random
+
+
+
+"""Données"""
+EMP=pd.read_csv("https://raw.githubusercontent.com/kilianguillon/Projet_statapp/main/data/EMP_deplacements_Charme.csv",sep=";", encoding='latin-1')
+EMP["HEURE_ARRIVEE"]=EMP["HEURE_ARRIVEE"].replace(',', '.', regex=True).astype(float)
+
+"""Fonctions"""
+
+def count_occ_pond(data, nom_var, nom_pond, taille=10):
+    value = data[nom_var].unique() #cette fonction est utile pour des variables prenant un nbre de valeurs finies
+    result_dict = {nom_var: [], 'Occurences pondérées': [], 'Proportion':[]} #à chaque valeur on associe la somme des occurences pondérées
+    N=data[nom_pond].replace(',', '.', regex=True).astype(float).sum()
+    for val in value :
+        somme_pond = data[data[nom_var] == val][nom_pond].replace(',', '.', regex=True).astype(float).sum() #replace car la base utilisée est "française"
+        result_dict[nom_var].append(val)
+        result_dict['Occurences pondérées'].append(somme_pond)
+        result_dict['Proportion'].append(round(100*somme_pond/N,2))
+    return pd.DataFrame(result_dict).sort_values('Occurences pondérées', ascending=False).reset_index(drop=True).head(taille)
+
+
+#On définit une fonction qui selon les probabilités ci-dessus, va produire le lieu de départ de n individus
+def loi_lieu_depart(n=1, p_domicile=70.49/100, p_rue=23.87/100, p_parking=3.90/100, p_entreprise=1.38/100, p_sans=0.36/100):
+    matrice_initiale = [random.uniform(0, 1) for k in range(n)]
+    for i in range(n):
+        if matrice_initiale[i] <= p_domicile:
+            matrice_initiale[i] = "Domicile"
+        elif matrice_initiale[i] <= p_domicile + p_rue:
+            matrice_initiale[i] = "Rue"
+        elif matrice_initiale[i] <= p_domicile + p_rue + p_parking:
+            matrice_initiale[i] = "Parking"
+        elif matrice_initiale[i] <= p_domicile + p_rue + p_parking + p_entreprise:
+            matrice_initiale[i] = "Entreprise"
+        else:
+            matrice_initiale[i] = "Sans"
+    
+    return matrice_initiale
+
+
+#On va créer un e fonction liée à count_occ_pound qui pour chaque plage horraire va nous envoyer la proba de Lieu_Arrivee pour chaque Lieu_Depart
+def transition_plage(plage):
+
+    #On crée les plages horraires :
+
+    EMP.loc[EMP["HEURE_ARRIVEE"].between(0, 11),"Plage_horraire"] = "00-11h"
+    EMP.loc[EMP["HEURE_ARRIVEE"].between(11, 14),"Plage_horraire"] = "11-14h"
+    EMP.loc[EMP["HEURE_ARRIVEE"].between(14, 17),"Plage_horraire"] = "14-17h"
+    EMP.loc[EMP["HEURE_ARRIVEE"].between(17, 24),"Plage_horraire"] = "17-00h"
+
+    Lieux = ["Domicile","Rue","Entreprise","Parking","Sans"] 
+    results=None
+    for Lieu in Lieux:
+        data = EMP[(EMP["Plage_horraire"] == plage) & (EMP["Lieu_Depart"] == Lieu)]
+        results_temp = count_occ_pond(data, 'Lieu_Arrivee', 'POND_JOUR').iloc[:, [0, 2]]
+
+        results_temp["Proportion"] = results_temp["Proportion"] / 100
+        results_temp.rename(columns={'Proportion': 'Proba'+'_'+Lieu}, inplace=True)
+
+
+        if results is None: #on fusionne toutes les proportions pour créer la matrice
+            results = results_temp
+        else:
+            results = pd.merge(results, results_temp, on='Lieu_Arrivee')
+
+    return results
+
+
+#Maintenant on va créer une fonction à laquelle on donne un lieu de départ et une heure de départ et qui nous donne un lieu d'arrivée
+#En suivant les proba obtenues ci-dessus
+def lieu_arrivee(lieu_depart, heure_depart): #juste l'heure, les minutes ne sont pas utiles
+    if heure_depart<11: #on trouve la plage horraire correspondante
+        plage="00-11h"
+    elif heure_depart<14 :
+        plage="11-14h"
+    elif heure_depart<17 :
+        plage="14-17h"
+    else:
+        plage="17-00h"
+
+    table=transition_plage(plage)[["Lieu_Arrivee", "Proba_"+lieu_depart]] #on prend les proba de déplacement pour cette plage et ce lieu de départ
+    
+    proba_parking = table.loc[table["Lieu_Arrivee"] == "Parking"].iloc[0,1]#proba d'aller dans un parking selon l'heure et le lieu de départ donnés.
+    proba_domicile = table.loc[table["Lieu_Arrivee"] == "Domicile"].iloc[0,1]
+    proba_rue = table.loc[table["Lieu_Arrivee"] == "Rue"].iloc[0,1]
+    proba_entreprise = table.loc[table["Lieu_Arrivee"] == "Entreprise"].iloc[0,1]
+    proba_sans = table.loc[table["Lieu_Arrivee"] == "Sans"].iloc[0,1]
+    
+    tirage=random.uniform(0, 1) #on construit la loi avec les probas obtenues
+    if tirage < proba_parking:
+        Lieu_Arrivee="Parking"
+    elif tirage < proba_parking + proba_domicile:
+        Lieu_Arrivee="Domicile"
+    elif tirage < proba_parking + proba_domicile + proba_rue:
+        Lieu_Arrivee="Rue"
+    elif tirage < proba_parking + proba_domicile + proba_rue + proba_entreprise:
+        Lieu_Arrivee="Entreprise"
+    else:
+        Lieu_Arrivee="Sans"
+    
+    return Lieu_Arrivee
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#Fonction finale pour notre première simulation (elle doit sortir la journée de déplacements d'un individu).
+#On reprend le format de la base EMP ? (une ligne = un déplacement)
+def simulation():
+    Jour=None
+
+    return Jour
+
+
+
+"""Script"""
